4d5bdc6899e4e0d4b7a50982fda251449cf10e3e
jannramco <uvaanuk@gmail.com>
2024-06-06 22:05:25 -0500

    actualización storage

--- js/app.js
+++ js/app.js
@@ -202,146 +202,164 @@ function actualizarTotalCarrito(){
 
 
 const productos = [
-    // Abrigos
+    
     {
         id: "590774",
         titulo: "falda corta dorada",
         imagen: ".img/falda_corta_dorada.png",
-        precio: 23999
+        precio: 23999,
+        precioItemAnt: 29999,
+        sku:590774
+
     },
     {
         id: "591721",
         titulo: "pantalón cargo verde",
         imagen: ".img/jean_bota_Amp_verde.png",
-        precio: 83999
+        precio: 83999,
+        sku: 591721
     },
     {
         id: "597326",
         titulo: "chaqueta animal print",
         imagen: ".img/chaqueta_animal_print.png",
-        precio: 95000
+        precio: 95000,
+        sku: 597326
     },
     {
         id: "597489",
         titulo: "Chaqueta denim azul medio",
         imagen: "img/chaqueta_jean_azul_medio.png",
-        precio: 88000
+        precio: 88000,
+        sku: 597489
     },
     {
         id: "596403",
         titulo: "jean bota amplia azul",
         imagen: ".img/jean_bota_amplia.png",
-        precio: 79000
+        precio: 79000,
+        sku: 596403
     },
    {
         id: "593867",
         titulo: "vestido largo marfil",
         imagen: "./img/vestido_largo_marfil.png",
-        precio: 75500
+        precio: 75500,
+        sku: 593867
     },
     {
         id: "599010",
         titulo: "vestido corto denim azul",
         imagen: "./img/vestido_corto_jean_2.png",
-        precio: 71900
+        precio: 71900,
+        sku: 599010
     },
     {
         id: "597651",
         titulo: "vestido largo multicolor",
         imagen: "./img/vestido_largo_multicolor.png",
-        precio: 79800
+        precio: 79800,
+        sku: 597651
     },
     {
         id: "597654",
         titulo: "conjunto croptop - pantalón lila",
         imagen: "./img/conjunto_croptop_pantalon.png",
-        precio: 75800
+        precio: 75800,
+        sku: 597654
     },
     {
         id: "camiseta-05",
         titulo: "Camiseta 05",
         imagen: "./img/camisetas/05.jpg",
-        precio: 1000
+        precio: 1000,
+        sku: 59
     },
     {
         id: "camiseta-06",
         titulo: "Camiseta 06",
         imagen: "./img/camisetas/06.jpg",
-        categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 91
     },
     {
         id: "camiseta-07",
         titulo: "Camiseta 07",
         imagen: "./img/camisetas/07.jpg",
-        categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 17
     },
     {
         id: "camiseta-08",
         titulo: "Camiseta 08",
         imagen: "./img/camisetas/08.jpg",
-        categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 72
     },
-    // Pantalones
     {
         id: "pantalon-01",
         titulo: "Pantalón 01",
         imagen: "./img/pantalones/01.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 21
     },
     {
         id: "pantalon-02",
         titulo: "Pantalón 02",
         imagen: "./img/pantalones/02.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 1
     },
     {
         id: "pantalon-03",
         titulo: "Pantalón 03",
         imagen: "./img/pantalones/03.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 5
     },
     {
         id: "pantalon-04",
         titulo: "Pantalón 04",
         imagen: "./img/pantalones/04.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 9
     },
     {
         id: "pantalon-05",
         titulo: "Pantalón 05",
         imagen: "./img/pantalones/05.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
+        precio: 1000,
+        sku: 7
     }
 ];
+
+const contenedorItems=document.querySelector("#contenedorItems");
+
+
+
+
+function cargarItems ()
+{ 
+    productos.forEach(productos => {
+
+        let div= document.createElement("div");
+        div.classList.add("productos");
+        div.innerHTML= `
+            <img class="img-item" src="${productos.imagen}" alt="${productos.titulo}" class="img-item">
+            <span class="titulo-item">"${productos.titulo}"</span>
+                <div class="precio_items">
+                    <p class="precio-item"> $${productos.precio}</p>
+                    <p class="precio-item-ant"> $${productos.precioItemAnt}}</p>
+                </div>
+                <h4 id="item-info-sku">
+                <strong><em>${productos.sku}</em></strong>
+            </h4>
+            <button class="boton-item" id="${productos.id}">Añadir al Carrito</button>
+            `;
+
+            contenedorProductos.append(div);
+        }
+    )
+}
+
+cargarItems ();
